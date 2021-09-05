0b1c52d5de2eb37ea3965b529282608319392dfb
liuxiao <liuxiao2103@qq.com>
2021-09-05 23:35:11 +0800

    应用

---
+++ elasticsearch_demo1/src/main/java/com/liuxiaocs/ElasticsearchDemo1Application.java
@@ -0,0 +1,13 @@
+package com.liuxiaocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchDemo1Application.class, args);
+    }
+
+}
