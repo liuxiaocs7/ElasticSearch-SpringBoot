e8236731d012447504bf6f25ac6d1c024f9a3952
liuxiao <liuxiao2103@qq.com>
2021-09-05 23:35:23 +0800

    实体类

---
+++ elasticsearch_demo1/src/main/java/com/liuxiaocs/entity/Book.java
@@ -0,0 +1,22 @@
+package com.liuxiaocs.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Book {
+    private String id;
+    private String name;
+    private Integer age;
+    private Date bir;
+    private String content;
+    private String address;
+}
