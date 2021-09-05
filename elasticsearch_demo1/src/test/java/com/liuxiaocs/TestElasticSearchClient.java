7e203ee5ed99b7430b5d5180fd5a97c083583a07
liuxiao <liuxiao2103@qq.com>
2021-09-05 23:35:48 +0800

    ES基础操作模板

---
+++ elasticsearch_demo1/src/test/java/com/liuxiaocs/TestElasticSearchClient.java
@@ -0,0 +1,27 @@
+package com.liuxiaocs;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class TestElasticSearchClient {
+
+    @Test
+    public void testInit() throws UnknownHostException {
+        // 创建ES客户端对象
+        TransportClient transportClient = new PreBuiltTransportClient(Settings.EMPTY);
+        // 设置操作ES服务主机和端口
+        transportClient.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.200.129"), 9300));
+
+        // 操作
+
+        // 释放
+        transportClient.close();
+
+    }
+}
